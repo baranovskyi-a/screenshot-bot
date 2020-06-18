3c6c84cb03133b94e81a2518a18297a72574ae72
Andrii <baranovskyi.and@gmail.com>
2020-06-19 00:56:00 +0300

    impl bonus part

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+from selenium import webdriver
+
+MAX_WIDTH = 1920
+MAX_HEIGHT = 8080
+WEB_DRIVER = webdriver.Safari
+PAGE_LOAD_TIMEOUT = 10
+REMOVE_FIXED = True
+
 TOKEN = ''
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from aiogram import Bot, Dispatcher, types, executor
-from config import TOKEN
+import config
 import validators
-from selenium import webdriver
 from PIL import Image
 from io import BytesIO
 import base64
 from time import sleep
 
-bot = Bot(TOKEN)
+
+bot = Bot(config.TOKEN)
 dp = Dispatcher(bot)
 
 
@@ -23,18 +23,12 @@ def remove_fixed_elements(driver, parent_node):
             remove_fixed_elements(driver, element)
 
 
-@dp.message_handler()
-async def make_screen(message: types.Message):
-    if not validators.url(message.text):
-        await bot.send_message(message.from_user.id, 'Url is not valid')
-        return None
-
-    driver = webdriver.Safari()
-    driver.set_page_load_timeout(10)
-    driver.get(message.text)
-    max_width = 1920
-    max_height = 8080
-    screen_width = max_width
+def get_b64_screen_from_url(url, remove_fixed=False):
+    driver = config.WEB_DRIVER(quiet=True)
+    driver.set_page_load_timeout(config.PAGE_LOAD_TIMEOUT)
+    driver.get(url)
+    sleep(1)
+    screen_width = config.MAX_WIDTH
     screen_height = driver.execute_script('return Math.max('
                                           'document.body.scrollHeight, '
                                           'document.body.offsetHeight, '
@@ -42,21 +36,34 @@ async def make_screen(message: types.Message):
                                           'document.documentElement.scrollHeight, '
                                           'document.documentElement.offsetHeight);')
     driver.set_window_size(screen_width, screen_height)
-    body = driver.find_element_by_tag_name('body')
-    # remove_fixed_elements(driver, body)
-
+    if remove_fixed:
+        body = driver.find_element_by_tag_name('body')
+        remove_fixed_elements(driver, body)
     b64str = driver.get_screenshot_as_base64()
     driver.quit()
-    im = Image.open(BytesIO(base64.b64decode(b64str)))
-    if im.size[0] > max_width or im.size[1] > max_height:
-        divider = max(im.size[0]/max_width, im.size[1]/max_height)
-        im = im.resize(size=(int(im.size[0]/divider), int(im.size[1]/divider)))
+    return b64str
+
+
+def get_bytesio_image_from_b64(b64):
+    im = Image.open(BytesIO(base64.b64decode(b64)))
+    if im.size[0] > config.MAX_WIDTH or im.size[1] > config.MAX_HEIGHT:
+        divider = max(im.size[0] / config.MAX_WIDTH, im.size[1] / config.MAX_HEIGHT)
+        im = im.resize(size=(int(im.size[0] / divider), int(im.size[1] / divider)))
     bio = BytesIO()
     bio.name = 'image.png'
     im.save(bio, 'PNG')
     bio.seek(0)
-    await bot.send_photo(message.from_user.id, photo=bio)
+    return bio
 
+
+@dp.message_handler()
+async def make_screen(message: types.Message):
+    if not validators.url(message.text):
+        await bot.send_message(message.from_user.id, 'Url is not valid')
+        return None
+    b64str = get_b64_screen_from_url(url=message.text, remove_fixed=config.REMOVE_FIXED)
+    bio = get_bytesio_image_from_b64(b64=b64str)
+    await bot.send_photo(message.from_user.id, photo=bio)
 
 
 if __name__ == '__main__':
